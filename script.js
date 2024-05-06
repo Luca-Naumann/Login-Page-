469a6c3341509c8eac520f0d0c78acd44dd11060
Luca-Naumann <159116934+Luca-Naumann@users.noreply.github.com>
2024-05-06 20:44:57 +1000

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+document.addEventListener("contextmenu", function(event){
+event.preventDefault();
+alert('Right Click is Disabled');    
+}, false);
